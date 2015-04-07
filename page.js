4097e70d5c740b7658acbfdecb15b8d2627cbccc
Miller Augusto <miller.augusto@gmail.com>
2015-04-07 17:31:11 -0300

    Update page.js

--- page.js
+++ page.js
@@ -7,12 +7,17 @@ var page = {
       theme : 'monokai'
     }),
     
-    showSelected : function(file){
+    showResult : function(){
+        $('#codeArea').removeClass('col-md-12');
+        $('#codeArea').addClass('col-md-8');
+        $('#treeArea').fadeIn('slow');
+    },
+    
+    setSelected : function(file){
         console.log(file);
     },
     
     setTree : function(json){
-        console.log(json);
         $('#filesTree').jstree({ 'core' : {
             "multiple" : false,
             'data' : json
@@ -21,35 +26,41 @@ var page = {
             for(i = 0, j = data.selected.length; i < j; i++) {
               r.push(data.instance.get_node(data.selected[i]).text);
             }
-            page.showSelected(r[0]);
+            page.setSelected(r[0]);
         });
     },
     
+    getGenerator : function(){
+        var data = {
+            tree : [
+               { "id" : "resources", "parent" : "#", "text" : "resources", "state" : { "opened" : true } },
+               { "id" : "js", "parent" : "resources", "text" : "js", "state" : { "opened" : true } },
+               { "id" : "controllers", "parent" : "js", "text" : "controllers" },
+               { "id" : "models", "parent" : "js", "text" : "models" },
+               { "id" : "services", "parent" : "js", "text" : "services" },
+               
+               { "id" : "fileA", "parent" : "controllers", "text" : "fileA.js", "icon" : "glyphicon glyphicon-leaf" },
+               { "id" : "fileB", "parent" : "controllers", "text" : "fileB.js", "icon" : "glyphicon glyphicon-leaf" },
+               { "id" : "fileC", "parent" : "controllers", "text" : "fileC.js", "icon" : "glyphicon glyphicon-leaf" },
+               
+               { "id" : "fileD", "parent" : "models", "text" : "fileD.js", "icon" : "glyphicon glyphicon-file" },
+               { "id" : "fileE", "parent" : "models", "text" : "fileE.js", "icon" : "glyphicon glyphicon-file" },
+               { "id" : "fileF", "parent" : "models", "text" : "fileF.js", "icon" : "glyphicon glyphicon-file" },
+               
+               { "id" : "fileG", "parent" : "services", "text" : "fileG.js", "icon" : "glyphicon glyphicon-fire" },
+               { "id" : "fileH", "parent" : "services", "text" : "fileH.js", "icon" : "glyphicon glyphicon-fire" },
+               { "id" : "fileI", "parent" : "services", "text" : "fileI.js", "icon" : "glyphicon glyphicon-fire" },
+               
+               { "id" : "App", "parent" : "resources", "text" : "App.js", "icon" : "glyphicon glyphicon-fire", "state" : { "selected" : true } }
+            ]
+        };
+        return data;
+    },
+    
     run : function(){
-        $('#codeArea').removeClass('col-md-12');
-        $('#codeArea').addClass('col-md-8');
-        $('#treeArea').fadeIn('slow');
-        page.setTree([
-           { "id" : "resources", "parent" : "#", "text" : "resources", "state" : { "opened" : true } },
-           { "id" : "js", "parent" : "resources", "text" : "js", "state" : { "opened" : true } },
-           { "id" : "controllers", "parent" : "js", "text" : "controllers" },
-           { "id" : "models", "parent" : "js", "text" : "models" },
-           { "id" : "services", "parent" : "js", "text" : "services" },
-           
-           { "id" : "fileA", "parent" : "controllers", "text" : "fileA.js", "icon" : "glyphicon glyphicon-leaf" },
-           { "id" : "fileB", "parent" : "controllers", "text" : "fileB.js", "icon" : "glyphicon glyphicon-leaf" },
-           { "id" : "fileC", "parent" : "controllers", "text" : "fileC.js", "icon" : "glyphicon glyphicon-leaf" },
-           
-           { "id" : "fileD", "parent" : "models", "text" : "fileD.js", "icon" : "glyphicon glyphicon-file" },
-           { "id" : "fileE", "parent" : "models", "text" : "fileE.js", "icon" : "glyphicon glyphicon-file" },
-           { "id" : "fileF", "parent" : "models", "text" : "fileF.js", "icon" : "glyphicon glyphicon-file" },
-           
-           { "id" : "fileG", "parent" : "services", "text" : "fileG.js", "icon" : "glyphicon glyphicon-fire" },
-           { "id" : "fileH", "parent" : "services", "text" : "fileH.js", "icon" : "glyphicon glyphicon-fire" },
-           { "id" : "fileI", "parent" : "services", "text" : "fileI.js", "icon" : "glyphicon glyphicon-fire" },
-           
-           { "id" : "App", "parent" : "resources", "text" : "App.js", "icon" : "glyphicon glyphicon-fire", "state" : { "selected" : true } }
-        ]);        
+        var data = page.getGenerator();
+        page.showResult();
+        page.setTree(data.tree);
     },
     
     setEventlistner : function(){
