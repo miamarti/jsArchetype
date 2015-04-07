789e1fd9077b7e5203cc9b49f9ce1500f434a6df
Miller Augusto <miller.augusto@gmail.com>
2015-04-07 15:23:28 -0300

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,12 @@
-var page = {
-};
+$(document).on("pageload",function(){
+    editor = CodeMirror.fromTextArea(document.getElementById("condeConfig"), {
+      lineNumbers: true,
+      matchBrackets: true,
+      styleActiveLine: true,
+      matchBrackets: true,
+      theme : 'monokai'
+    });
+    
+    var page = {
+    };
+});
