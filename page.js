d5cea2636ee3e9c58080d9edadf3e05c8eb94a53
Miller Augusto <miller.augusto@gmail.com>
2015-04-07 15:40:09 -0300

    Update page.js

--- page.js
+++ page.js
@@ -10,15 +10,19 @@ var page = {
     }),
     
     setTree : function(json){
+        console.log(json);
         $('#filesTree').jstree({ 'core' : {
             'data' : json
         } });
+    },
+    
+    main : function(){
+        page.setTree([
+           { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+           { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+           { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+           { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" }
+        ]);
     }
 };
-
-page.setTree([
-               { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
-               { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
-               { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
-               { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
-            ]);
+page.main();
