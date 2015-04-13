b582beee16d20fa9e8b8d20afe305de5ded99b69
Miller Augusto <miller.augusto@gmail.com>
2015-04-13 10:16:29 -0300

    Update jsArchetype.v1.0.1.js

--- jsArchetype.v1.0.1.js
+++ jsArchetype.v1.0.1.js
@@ -265,20 +265,14 @@ var jsArchetype = {
 		};
 		appData.jsCode  = 'var ' + jsArchetype.camelCase(jsArchetype.config.projectName) + ' = angular.module(\'' + jsArchetype.camelCase(jsArchetype.config.projectName) + '\', ' + JSON.stringify(jsArchetype.config.plugins) + ');\n';
 		appData.jsCode += jsArchetype.camelCase(jsArchetype.config.projectName) + '.config([ \'$stateProvider\', \'$urlRouterProvider\', \'$httpProvider\', function($stateProvider, $urlRouterProvider, $httpProvider) {\n';
-		appData.jsCode += '	var stateBean = function(url, template, data) {\n';
-		appData.jsCode += '	    this.url = url;\n';
-		appData.jsCode += '	    this.templateUrl = template;\n';
-		appData.jsCode += '	    this.data = data;\n';
-		appData.jsCode += '	};\n';
 		appData.jsCode += '\n';
-		
 		var routerInit = true;
 		jsArchetype.config.routers.forEach(function(router){
 			if(routerInit){
 				routerInit = false;
 				appData.jsCode += '	$urlRouterProvider.otherwise(\'' + router.state + '\');\n';
 			}
-			appData.jsCode += '	$stateProvider.state(\'' + router.state + '\', new stateBean(\'' + router.hash + '\', \'' + router.template + '\', ' + JSON.stringify(router.data) + '));\n';
+			appData.jsCode += '	$stateProvider.state(\'' + router.state + '\', {url : \'' + router.hash + '\', templateUrl : \'' + router.template + '\', data : \'' + JSON.stringify(router.data) + '\'}));\n';
 		});
 		
 		appData.jsCode += '} ]);\n';
